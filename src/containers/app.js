3ac998714460ce2f79d44314483a9f629b152ef6
Anthony <anthonyilinykh@gmail.com>
2016-08-12 17:08:15 +0300

    Drawer works!

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,50 +1,32 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../store/configureStore';
-import { Actions, Scene, Router, DefaultRenderer } from 'react-native-router-flux';
+import { Actions, Scene, Router, Reducer } from 'react-native-router-flux';
 import ProductList from './productList';
-import SideMenu from './sideMenu';
 import ProductView from '../components/productView';
-import { Drawer } from 'native-base';
-
-export default class extends Component {
-  render(){
-    const state = this.props.navigationState;
-    const children = state.children;
-    return (
-      <Drawer
-        ref="navigation"
-        open={state.open}
-        onOpen={()=>Actions.refresh({key:state.key, open: true})}
-        onClose={()=>Actions.refresh({key:state.key, open: false})}
-        type="displace"
-        content={<SideMenu />}
-        tapToClose={true}
-        openDrawerOffset={0.2}
-        panCloseMask={0.2}
-        negotiatePan={true}
-        tweenHandler={(ratio) => ({
-         main: { opacity:Math.max(0.54,1-ratio) }
-      })}>
-        <DefaultRenderer navigationState={children[0]} onNavigate={this.props.onNavigate} />
-      </Drawer>
-    );
-  }
-}
-
+import NavigationDrawer from '../components/NavigationDrawer';
 
 const store = configureStore();
+
+const reducerCreate = params => {
+  const defaultReducer = new Reducer(params);
+  return (state, action) => {
+    console.log('ACTION:', action);
+    return defaultReducer(state, action);
+  };
+};
 
 export default class App extends Component {
   render() {
     return(
       <Provider store={store}>
-        <Router>
-          <Scene key="root" leftTitle="SideMenu" onLeft={() => Actions.refresh({key: 'drawer', open: value => !value })}>
-            <Scene key="productList" component={ProductList} title="Меню" initial="true"/>
-            <Scene key="productView" component={ProductView} title="Бургер"/>
-            <Scene key="drawer" component={Drawer} open={false}>
-              <Scene key="sideMenu" component={SideMenu} hideNavBar="true"/>
+        <Router createReducer={reducerCreate}>
+          <Scene key="root">
+            <Scene key="drawer" component={NavigationDrawer}>
+              <Scene key="main" drawerImage={require("../images/navicon.png")}>
+                <Scene key="productList" component={ProductList} title="Меню" initial="true"/>
+                <Scene key="productView" component={ProductView} title="Бургер"/>
+              </Scene>
             </Scene>
           </Scene>
         </Router>
--- src/containers/sideMenu.js
+++ src/containers/sideMenu.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
-import { StyleSheet, ListView, RefreshControl, View, Text, Image } from 'react-native';
-import { Drawer } from 'native-base';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { KB_ORANGE } from '../constants';
 
 export default class SideMenu extends Component {
 
-
-
   render() {
     return (
-      <View>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-        <Text>Side Menu</Text>
-      </View>
+      <ScrollView style={styles.container} scrollsToTop={false}>
+        <View style={styles.separator}/>
+        <Text style={styles.row}>МЕНЮ</Text>
+        <View style={styles.separator}/>
+        <Text style={styles.row}>КАК НАС НАЙТИ</Text>
+        <View style={styles.separator}/>
+        <View style={{padding: 15, flexDirection:'row'}}>
+          <Icon name='vk' size={25} color='white'/>
+          <Icon name='instagram' size={25} color='white' style={{paddingLeft: 25}}/>
+        </View>
+        <View style={styles.separator}/>
+        <View style={styles.headerContainer}>
+          <Text style={styles.header}>Последние заказы</Text>
+        </View>
+        <Text style={styles.row}>Нет заказов</Text>
+        <View style={styles.separator}/>
+      </ScrollView>
     );
   }
-
-
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    backgroundColor: KB_ORANGE,
+  },
+  separator: {
+    borderTopWidth: 1,
+    borderTopColor: 'white',
+    opacity:.4
+  },
+  row: {
+    color: 'white',
+    padding: 15,
+  },
+  headerContainer: {
+    backgroundColor:'#fccfb0',
+    marginTop:15,
+  },
+  header: {
+    padding:10,
+    paddingLeft:15,
+  }
+});
