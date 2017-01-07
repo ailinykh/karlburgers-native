2245fdc444e20cbc581d6d693f96a91ef130d38a
Anthony Ilinykh <anthonyilinykh@gmail.com>
2017-01-07 21:33:59 +0300

    About page links bugfix

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,7 @@ import { Card, CardItem } from 'native-base';
 import Icon from 'react-native-vector-icons/Ionicons';
 import NavigationBar from './NavigationBar';
 import { KB_PHONE_NUMBER, KB_FULL_PHONE_NUMBER } from '../constants';
-/*<MapView.Marker
-  coordinate={{latitude: 52.961597, longitude: 36.064426}}
-  title='{marker.title}'
-  description='{marker.description}'
-/>*/
+
 export default class About extends Component {
 
   render() {
@@ -19,21 +15,22 @@ export default class About extends Component {
           style={styles.map}
           initialRegion={{latitude: 52.961597, longitude: 36.064426, latitudeDelta: 0.002, longitudeDelta: 0.002}}
           >
+          <MapView.Marker
+            coordinate={{latitude: 52.961597, longitude: 36.064426}}
+            title='Бургерная Карл Маркс'
+            description='Карачевская 12/3'
+          />
         </MapView>
         <Card style={{margin:15}}>
           <CardItem style={styles.card}>
             <Icon name="ios-home" size={21}/><Text style={styles.cardText}>Ежедневно (11:30 - 22:30)</Text>
           </CardItem>
-          <TouchableOpacity onPress={this._openPhone}>
-            <CardItem style={styles.card}>
-              <Icon name="md-call" size={23}/><Text style={styles.cardTextHighlited}>+7 (4862) {KB_PHONE_NUMBER}</Text>
-            </CardItem>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this._openMap}>
-            <CardItem style={styles.card}>
-              <Icon name="md-pin" size={25}/><Text style={styles.cardTextHighlited}>Карачевская 12/3</Text>
-            </CardItem>
-          </TouchableOpacity>
+          <CardItem style={styles.card} onPress={this._openPhone}>
+            <Icon name="md-call" size={23}/><Text style={styles.cardTextHighlited}>+7 (4862) {KB_PHONE_NUMBER}</Text>
+          </CardItem>
+          <CardItem style={styles.card} onPress={this._openMap}>
+            <Icon name="md-pin" size={25}/><Text style={styles.cardTextHighlited}>Карачевская 12/3</Text>
+          </CardItem>
         </Card>
       </ScrollView>
     );
@@ -59,6 +56,7 @@ const styles = StyleSheet.create({
   map: {
     height: 250,
     width: Dimensions.get('window').width,
+    marginBottom: 15,
   },
   card: {
     padding: 15,
